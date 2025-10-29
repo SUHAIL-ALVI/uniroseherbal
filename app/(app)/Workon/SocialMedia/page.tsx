19c453dd4e46bb5f295c35a70487e6ac455a6118
Suhai Alvi <alvisuhail3@gmail.com>
2025-10-29 20:17:48 +0530

    insta changes

--- app/(app)/Workon/SocialMedia/page.tsx
+++ app/(app)/Workon/SocialMedia/page.tsx
@@ -2,7 +2,7 @@
 
 import { motion } from "framer-motion";
 import Image from "next/image";
-import { Facebook, Twitter, Instagram } from "lucide-react";
+import { Facebook, Twitter } from "lucide-react";
 
 export default function SocialComingSoon() {
   return (
